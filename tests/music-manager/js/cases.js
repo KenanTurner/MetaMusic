ef63c4df52c467782dc4f15a5b1b10c7d0558378
coolspykee <coolspykee@satx.rr.com>
2021-07-03 19:41:08 -0500

    Updated tests

--- tests/music-manager/js/cases.js
+++ tests/music-manager/js/cases.js
@@ -1,22 +1,124 @@
 import T from '../../shared/test.js';
 export default class TestCases extends T{
-	static pass(){
-		return Promise.resolve("Done");
+	static constructor(MusicManager,players,album){
+		MusicManager.players = players;
+		let mm = new MusicManager(); //default constructor
+		let nn = new MusicManager(mm);
+		let oo = mm.clone();
+		let pp = MusicManager.fromJSON(JSON.stringify(mm))
+		if(!mm.equals(nn)) throw new Error("Bad constructor!");
+		if(!mm.equals(oo)) throw new Error("Bad clone method!");
+		if(!mm.equals(pp)) throw new Error("Bad fromJson method!");
+		return mm.waitForEvent('ready')
+		.then(mm.chain('destroy'))
+		.then(nn.chain('destroy'))
+		.then(oo.chain('destroy'))
+		.then(pp.chain('destroy'))
+		//TODO add case for non default constructor
 	}
-	static fail(){
-		return Promise.reject("Done");
+	static playPause(MusicManager,players,album){
+		MusicManager.players = players;
+		let mm = new MusicManager();
+		mm.push(album);
+		return mm.waitForEvent('ready')
+		.then(mm.chain('play')) //play
+		.then(mm.chain('pause'))
+		.then(mm.chain('pause'))
+		.then(mm.chain('play'))
+		.then(mm.chain('play'))
+		.then(mm.chain('pause'))
+		.finally(mm.chain('destroy'));
 	}
-	static error(){
-		throw new Error("AHHH");
-		return Promise.resolve("Done");
+	static next(MusicManager,players,album){
+		MusicManager.players = players;
+		let mm = new MusicManager();
+		mm.push(album);
+		let wait = function(time){
+			return function(){
+				return new Promise(function(res,rej){
+					setTimeout(res,time);
+				});
+			}
+		}
+		return mm.waitForEvent('ready')
+		.then(mm.chain('setVolume',0))
+		.then(mm.chain('play')) //play t1
+		.then(wait(500))
+		.then(mm.chain('next')) //t2
+		.then(wait(500))
+		.then(mm.chain('next')) //t3
+		.then(wait(500))
+		.then(mm.chain('next')) //t4
+		.then(wait(500))
+		.then(mm.chain('next')) //t1
+		.then(mm.chain('next')) //t2
+		.then(mm.chain('next')) //t3
+		.then(mm.chain('next')) //t4
+		.then(mm.chain('next')) //t1
+		.finally(mm.chain('destroy'));
 	}
-	static bad_return(){
-		console.log("Oops");
-		return true;
+	//TODO fix shuffle case
+	static shuffle(MusicManager,players,album){
+		MusicManager.players = players;
+		let mm = new MusicManager();
+		mm.push(album);
+		let copy = mm.clone();
+		if(!copy.equals(mm)) throw new Error("Cloning fails to keep track order!");
+		mm.shuffle();
+		if(!copy.equals(mm)) throw new Error("Shuffling fails?");
+		return mm.waitForEvent('ready')
+		.then(mm.chain('destroy'))
+		.then(copy.chain('destroy'))
 	}
-	static timeout(){
-		return new Promise(function(res,rej){
-			setTimeout(res,15000);
-		});
+	
+	static subs(MusicManager,players,album){
+		MusicManager.players = players;
+		let mm = new MusicManager();
+		mm.push(album);
+		var check = {
+			loaded:false,
+			play:false,
+			pause:false,
+			ended:false,
+			error:false,
+			timeupdate:false,
+			volumechange:false
+		}
+		var f = function(evt){
+			console.log(evt);
+			check[evt.type] = true;
+		}
+		mm.subscribe('loaded',f);
+		mm.subscribe('play',f);
+		mm.subscribe('pause',f);
+		mm.subscribe('ended',f);
+		mm.subscribe('error',f);
+		mm.subscribe('timeupdate',f);
+		mm.subscribe('volumechange',f);
+		return mm.waitForEvent('ready')
+		.then(mm.chain('setVolume',0))
+		.then(mm.chain('play'))
+		.then(mm.chain('next'))
+		.then(mm.chain('next'))
+		.then(mm.chain('pause'))
+		.then(mm.chain('next'))
+		.then(mm.chain('next'))
+		.then(mm.chain('load',new players.HTML.Track({title:"Error",src:"http://e"})))
+		.then(function(){
+			throw new Error("This Error should not be thrown");
+		})
+		.catch(function(evt){
+			if(evt.message) throw evt;
+			return Promise.resolve()
+		})
+		.then(function(){
+			return new Promise(function(res,rej){
+				for(let evt in check){
+					if(!check[evt]) return rej(check);
+				}
+				return res("Finished");
+			});
+		})
+		.finally(mm.chain('destroy'));
 	}
 }
--- tests/music-manager/js/main.js
+++ tests/music-manager/js/main.js
@@ -5,6 +5,7 @@ ModuleManager.importModules({
 	"BC":['./src/plugins/BC/bandcamp.js'],
 	"SC":['./src/plugins/SC/soundcloud.js'],
 	"MM":['./src/music-manager.js'],
+	"Album":['./src/album.js'],
 }).then(function(obj){
 	//put in global scope for easier debugging
 	window.TestCases = obj.TestCases.default;
@@ -13,28 +14,45 @@ ModuleManager.importModules({
 	window.BC = obj.BC.default;
 	window.SC = obj.SC.default;
 	window.MusicManager = obj.MM.default;
+	window.Album = obj.Album.default;
 	console.log("Loaded");
+	
+	window.t1 = new HTML.Track({src:"https://v.redd.it/6m47mro5xpv51/DASH_audio.mp4",title:"Scott's Factory"});
+	window.t2 = new YT.Track({src:"https://www.youtube.com/watch?v=zhG7aorm0RI",title:"Maynard & Waynard"});
+	window.t3 = new BC.Track({src:"https://austinwintory.bandcamp.com/track/then-were-created-the-gods-in-the-midst-of-heaven",title:"Then were created the gods in the midst of Heaven"});
+	window.t4 = new SC.Track({src:"https://soundcloud.com/i-winxd/kirby-speedrun",title:"Trance Music for Kirby Speedrunning Game"});
+	window.f = function(evt){
+		console.log(evt);
+	}
 	
 	let start_btn = document.getElementById("start_btn");
 	start_btn.addEventListener("click",function(){ //need to wait for user interaction
 		tc.clear();
+		let p = {};
+		for(let player in players){
+			if(players[player]) p[player] = window[player];
+		}
 		for(let c in test_cases){
-			if(test_cases[c]) tc.add(c,[]);
+			if(test_cases[c]) tc.add(c,[MusicManager,p,a]);
 		}
 		let disable_console = document.getElementById("option-disable-console");
 		tc.runAll(disable_console.checked);
 	});
 	
-	
+	Album.players = {"HTML":HTML,"YT":YT,"SC":SC,"BC":BC};
+	MusicManager.players = {"HTML":HTML,"YT":YT,"SC":SC,"BC":BC};
+	window.a = new Album({title:"t",tracks:[t1,t2,t3,t4]});
 	window.tc = new TestCases();
-	window.mm = new MusicManager(HTML,YT,BC,SC);
+	window.mm = new MusicManager();
+	mm.push(t1,t2,t3,t4);
+	mm.subscribe('all',f);
 })
 let test_cases = {
-	"pass":true,
-	"fail":true,
-	"error":true,
-	"bad_return":true,
-	"timeout":true,
+	"constructor":true,
+	"playPause":true,
+	"shuffle":true,
+	"next":true,
+	'subs':true,
 }
 let players = {
 	"HTML":true,
@@ -42,30 +60,6 @@ let players = {
 	"BC":true,
 	"SC":true,
 }
+//from ../shared/options.js
 createOptions(test_cases,"test_cases");
 createOptions(players,"players");
-
-function updateOptions(evt){
-	let name = evt.target.id.substring(7);
-	this[name] = evt.target.checked;
-}
-
-function createOptions(options,id){
-	let el = document.getElementById(id);
-	while (el.firstChild) {
-		el.removeChild(el.firstChild);
-	}
-	Object.keys(options).forEach(function(name){
-		let input = document.createElement("INPUT");
-		let label = document.createElement("LABEL");
-		input.type = "checkbox";
-		input.id = "option-"+name;
-		input.name = "option-"+name;
-		input.checked = true;
-		input.onclick = updateOptions.bind(options);
-		label.for = "option-"+name;
-		label.innerText = name+": ";
-		el.appendChild(label);
-		el.appendChild(input);
-	});
-}
