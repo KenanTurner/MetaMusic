cc2fa501f98f3cab10e43c0055cd87002c3c8e53
coolspykee <coolspykee@satx.rr.com>
2021-07-30 19:59:16 -0500

    Changed to MetaMusic

--- examples/plugins/js/main.js
+++ examples/plugins/js/main.js
@@ -3,7 +3,7 @@ ModuleManager.importModules({
 	"YT":['./src/plugins/YT/youtube.js'],
 	"BC":['./src/plugins/BC/bandcamp.js'],
 	"SC":['./src/plugins/SC/soundcloud.js'],
-	"MM":['./src/music-manager.js'],
+	"MM":['./src/meta-music.js'],
 }).then(function(obj){
 	let HTML = obj.HTML.default;
 	let YT = obj.YT.default;
