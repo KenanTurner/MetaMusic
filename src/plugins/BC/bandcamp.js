a16a18e1aaea74932ded6076b358f88666445ec7
KenanTurner <coolspykee@satx.rr.com>
2021-12-22 00:18:49 -0600

    Added static member proxy_url

--- src/plugins/BC/bandcamp.js
+++ src/plugins/BC/bandcamp.js
@@ -1,5 +1,6 @@
 import HTML from '../HTML/html.js';
 export default class BC extends HTML{
+	static proxy_url = "../../src/plugins/BC/bandcamp-proxy.php";
 	static Track = class Track extends HTML.Track{
 		constructor(obj){
 			super(obj);
@@ -15,34 +16,37 @@ export default class BC extends HTML{
 			return new BC.Track(JSON.parse(json));
 		}
 	}
-	constructor(bc_php = "../../src/plugins/BC/loadBC.php"){
-		super();
-		this._bc_php = bc_php;
+	constructor(is_ready=true){
+		super(is_ready);
 	}
-	load(track){
-		if(!this.constructor._validTrack(track)) throw new Error("Invalid Filetype");
-		if(track.bc_url == track.src){
-			//this._player.src = 'data:audio/wav;base64,UklGRiQAAABXQVZFZm10IBAAAAABAAEAVFYAAFRWAAABAAgAZGF0YQAAAAA='; //stop playing silently
-			return fetch(this._bc_php, {
+	async load(track){
+		if(!this.constructor.isValidTrack(track)) throw new Error("Invalid Filetype");
+		if(track.bc_url !== track.src) return super.load(track);
+		let p = this.waitForEvent('loaded');
+		try{
+			let result = await fetch(this.constructor.proxy_url,{
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
 				body: JSON.stringify({href:track.bc_url}),
-			})
-			.then(function(r){return r.json()})
-			.then(function(obj){
-				if(Object.values(obj).length == 0) return Promise.reject("Invalid Url");
-				track.src = Object.values(obj)[0];
-				return this.load(track);
-			}.bind(this))
-			.catch(function(error){
-				return Promise.reject(this._publish('error'));
-			}.bind(this));
+			});
+			let obj = await result.json();
+			if(Object.values(obj).length === 0) throw new Error("Invalid BC url");
+			track.src = Object.values(obj)[0];
+			super.load(track);
+		}catch(e){
+			this.publish(new this.constructor.Event('error'));
 		}
-		return super.load(track);
+		return p;
 	}
-	static _validURL(url){
+	async seek(time){
+		let status = await this.getStatus();
+		let p = await super.seek(time);
+		if(time >= status.duration) this.publish(new this.constructor.Event('ended'));
+		return p;
+	}
+	/*static _validURL(url){
 		try{
 			let tmp = new URL(url);
 			let arr = tmp.hostname.split('.');
@@ -53,5 +57,5 @@ export default class BC extends HTML{
 		}catch(e){
 			return false;
 		}
-	}
+	}*/
 }
