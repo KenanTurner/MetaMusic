b527c0a53e9e695d935bff8bad1656168aa22ade
KenanTurner <coolspykee@satx.rr.com>
2021-12-22 02:11:01 -0600

    Added hidden js console

---
+++ examples/simple/js/enable-console.js
@@ -0,0 +1,12 @@
+import Console from '../../../tests/shared/console.js';
+
+window.top.console = new Console();
+window.top.console._div.classList.add('hidden');
+document.body.appendChild(window.top.console._div);
+console.log("Console Loaded");
+
+document.addEventListener("keyup", function(e){
+	if(e.key === '`' || e.keyCode === 192){
+		window.top.console._div.classList.toggle('hidden');
+	}
+});
