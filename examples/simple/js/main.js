1a5541e9ae950fdbec7621867b1f320d432699f1
KenanTurner <coolspykee@satx.rr.com>
2021-12-22 01:51:11 -0600

    Fixed example

--- examples/simple/js/main.js
+++ examples/simple/js/main.js
@@ -1,33 +1,30 @@
-ModuleManager.importModules({
-	"HTML":['./src/plugins/HTML/html.js'],
-}).then(function(obj){
-	let HTML = obj.HTML.default;
-	console.log("Loaded");
+import HTML from '../../../src/plugins/HTML/html.js';
 	
-	let load_btn = document.getElementById("load");
-	let play_btn = document.getElementById("play");
-	let pause_btn = document.getElementById("pause");
-	let stop_btn = document.getElementById("stop");
-	let src_box = document.getElementById("src");
-	let track = new HTML.Track({src:src_box.value,title:"title"});
-	window.html = new HTML();
+let load_btn = document.getElementById("load");
+let play_btn = document.getElementById("play");
+let pause_btn = document.getElementById("pause");
+let stop_btn = document.getElementById("stop");
+let src_box = document.getElementById("src");
+
+let track = new HTML.Track({src:src_box.value,title:"title"});
+window.html = new HTML();
+html.subscribe({type:'error',callback:function(err){
+	console.error(err);
+	alert("There was an error playing the requested file");
+}});
+
+html.load(track);
+load_btn.addEventListener('click',function(){
+	track.src = src_box.value;
 	html.load(track);
-	load_btn.addEventListener('click',function(){
-		track.src = src_box.value;
-		html.load(track)
-	});
-	play_btn.addEventListener('click',function(){
-		html.play()
-	});
-	pause_btn.addEventListener('click',function(){
-		html.pause()
-	});
-	stop_btn.addEventListener('click',function(){
-		html.stop()
-	});
-	let error = function(err){
-		console.log(err);
-		alert("There was an error playing the requested file");
-	}
-	html.subscribe('error',error);
-})
+});
+play_btn.addEventListener('click',function(){
+	html.play();
+});
+pause_btn.addEventListener('click',function(){
+	html.pause();
+});
+stop_btn.addEventListener('click',function(){
+	html.stop();
+});
+
