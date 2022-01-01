afe1c7c2e894670a971e16fd517c4566142d2b53
KenanTurner <coolspykee@satx.rr.com>
2021-12-31 20:02:50 -0600

    Fixed Album and test cases

--- tests/albums/js/cases.js
+++ tests/albums/js/cases.js
@@ -1,99 +1,123 @@
-export default [
-	function constructor(Album,obj){
-		let a1 = new Album(obj);
-		let a2 = new Album(a1);
-		let a3 = new Album({title:"title"});
-		let a4 = new Album({title:"title",tracks:[]});
-		try{
-			let a5 = new Album();
-			return Promise.reject("Empty constructor is invalid");
-		}catch(e){}
-		return Promise.resolve();
+import Cases from '../../event-target/js/cases.js';
+let cases = Cases.map(function(f){
+    let g = async function({Album,album_args}){
+        return f({EventTarget:Album});
+    }
+    Object.defineProperty(g,"name",{value:f.name});
+    return g;
+})
+export default cases.concat([
+	async function constructor({Album,tracks}){
+		let a0 = new Album();
+		let a1 = new Album({tracks:[]});
+		throwIfNotEqual("Failed to create identical default albums!",a0,a1);
+		let a2 = new Album({title:"title"});
+		let a3 = new Album({title:"title",tracks:[]});
+		throwIfNotEqual("Failed to create identical empty albums!",a2,a3);
+		let a4 = new Album({title:"title",tracks});
+		let a5 = new Album(a4);
+		let a6 = new Album(a4.toJSON());
+		let a7 = a4.clone();
+		let a8 = Album.fromJSON(JSON.stringify(a4));
+		throwIfNotEqual("Failed to create identical albums!",a4,a5,a6,a7,a8);
+		if(!tracks.every(function(track,i,arr){
+			return track.equals(a4.tracks[i]);
+		})) throw new Error("Failed to include supplied tracks");
+		if(a4.title !== "title") throw new Error("Failed to include the original title");
 	},
-	function json(Album,obj){
-		let a1 = new Album(obj);
-		let jason_obj = a1.toJSON();
-		let a2 = new Album(jason_obj);
-		let json = JSON.stringify(a1);
-		let a3 = Album.fromJSON(json);
-		let a4 = a1.clone();
-
-		if(!a1.equals(a2)) throw new Error("Bad comparison");
-		if(!a1.equals(a3)) throw new Error("Bad comparison");
-		if(!a1.equals(a4)) throw new Error("Bad comparison");
-		if(!a2.equals(a3)) throw new Error("Bad comparison");
-		if(!a2.equals(a4)) throw new Error("Bad comparison");
-		if(!a3.equals(a4)) throw new Error("Bad comparison");
-		return Promise.resolve();
+	async function push({Album,tracks}){
+		let a1 = new Album({tracks});
+		let a2 = new Album();
+		let a3 = new Album();
+		let a4 = new Album();
+		a1.tracks.forEach(function(track){
+			a2.push(track);
+		});
+		a3.push(...a1.tracks);
+		a4.push(a1);
+		throwIfNotEqual("Failed to create identical albums!",a1,a2,a3,a4);
 	},
-	function tracks(Album,obj){
-		let a1 = new Album({title:"title"})
-		obj.tracks.forEach(function(track){
-			a1.push(track);
-		});
-		obj.tracks.forEach(function(track){
-			if(!a1.has(track)) throw new Error("Track is missing");
-		});
-		if(a1.length != obj.tracks.length) throw new Error("Mismatched lengths");
-		obj.tracks.forEach(function(track){
-			a1.remove(track);
-		});
-		if(a1.length > 0) throw new Error("Failed to remove track");
-		a1 = new Album(obj);
+	async function clear({Album,tracks}){
+		let a0 = new Album();
+		let a1 = new Album({tracks:[]});
+		a0.clear();
 		a1.clear();
-		if(a1.length > 0) throw new Error("Failed to clear");
-		return Promise.resolve();
+		throwIfNotEqual("Failed to create identical default albums!",a0,a1);
+		if(a0.tracks.length !== 0) throw new Error("Failed to clear all tracks");
+		let a2 = new Album({title:"title"});
+		let a3 = new Album({title:"title",tracks:[]});
+		a2.clear();
+		a3.clear();
+		throwIfNotEqual("Failed to create identical empty albums!",a2,a3);
+		if(a2.tracks.length !== 0) throw new Error("Failed to clear all tracks");
+		let a4 = new Album({title:"title",tracks});
+		let a5 = new Album(a4);
+		let a6 = new Album(a4.toJSON());
+		let a7 = a4.clone();
+		let a8 = Album.fromJSON(JSON.stringify(a4));
+		a4.clear();
+		a5.clear();
+		a6.clear();
+		a7.clear();
+		a8.clear();
+		throwIfNotEqual("Failed to create identical albums!",a4,a5,a6,a7,a8);
+		if(a4.tracks.length !== 0) throw new Error("Failed to clear all tracks");
 	},
-	function events(Album,obj){
-		let check = {
-			add:false,
-			sort:false,
-			remove:false,
-			clear:false,
-		}
-		let f = function(evt){
-			check[evt.type] = true;
-		}
-		let g = function(evt){console.log(evt)}
-		let a1 = new Album({title:"title"})
-		a1.subscribe('all',g);
-		a1.subscribe('add',f);
-		a1.subscribe('sort',f);
-		a1.subscribe('remove',f);
-		a1.subscribe('clear',f);
-		a1.push(...obj.tracks);
-		a1.sort("title");
-		a1.remove(obj.tracks[0]);
-		a1.clear();
-		for(let evt in check){
-			if(!check[evt]) return Promise.reject(check);
-		}
-		return Promise.resolve();
-	},
-	function getInfo(Album,obj){
-		let a1 = new Album(obj);
-		let info = a1.getInfo("title");
-		if(info.length != obj.tracks.length) throw new Error("Missing info");
-		info = a1.getInfo("src");
-		if(info.length != obj.tracks.length) throw new Error("Missing info");
-		info = a1.getInfo("this_key_does_not_exist");
-		if(info.length != 0) throw new Error("Returned a non-empty array");
-		return Promise.resolve();
-	},
-	function validTrack(Album,obj){
-		console.log(Album.players);
-		let tmp = {title:"title",src:"src"};
-		let v = Album._validTrack(tmp);
-		if(v) throw new Error("Object is not a valid track");
-		Object.values(Album.players).forEach(function(player){
-			let track = new player.Track(tmp);
-			v = Album._validTrack(track);
-			if(!v) throw new Error("Track is invalid");
+	async function insert({Album,tracks}){
+		let a1 = new Album({tracks});
+		let a2 = new Album();
+		let a3 = new Album();
+		let a4 = new Album();
+		a1.tracks.forEach(function(track,i){
+			a2.insert(i,track);
 		});
-		return Promise.resolve();
+		a3.insert(0,...a1.tracks);
+		a4.insert(0,a1);
+		throwIfNotEqual("Failed to create identical albums!",a1,a2,a3,a4);
+		for(let i = 0; i < a1.tracks.length; i++){
+			a2.insert(i*2,a1.tracks[i]); //aa,bb,cc,dd
+			a3.insert(i+a1.tracks.length,a1.tracks[i]); //abcd,abcd
+			a4.insert(a1.tracks.length-i,a1.tracks[i]); //a,b,c,d,d,c,b,a
+		}
+		for(let i = 0; i < a1.tracks.length; i++){
+			if(!a2.tracks[i*2].equals(a2.tracks[i*2+1])) throw new Error("Failed to insert at the correct index!");
+			if(!a3.tracks[i].equals(a3.tracks[i+a1.tracks.length])) throw new Error("Failed to insert at the correct index!");
+			if(!a4.tracks[i].equals(a4.tracks[a4.tracks.length-i-1])) throw new Error("Failed to insert at the correct index!");
+		}
 	},
-	function sort(Album,obj){
-		let a1 = new Album(obj);
+	async function remove({Album,tracks}){
+		let a0 = new Album();
+		let a1 = new Album({tracks});
+		a1.remove(a1);
+		throwIfNotEqual("Failed to remove self!",a0,a1);
+		a1.push(...tracks);
+		for(let i = 0; i < tracks.length; i++){
+			a1.remove(a1.tracks[0]);
+		}
+		throwIfNotEqual("Failed to remove singular track",a0,a1);
+		a1.push(...tracks);
+		a1.remove(...tracks);
+		throwIfNotEqual("Failed to remove multiple tracks",a0,a1);
+		a1.push(...tracks);
+		a0.push(...tracks.slice(0,tracks.length-2));
+		a1.remove(...tracks.slice(tracks.length-2));
+		throwIfNotEqual("Failed to remove subset of tracks",a0,a1);
+	},
+	async function shuffle({Album,tracks}){
+		let a1 = new Album({tracks});
+		let arr = Array.from({length: 64}, function(){
+			let tmp = a1.clone();
+			tmp.shuffle();
+			return tmp;
+		});
+		let result = arr.some(function(album){
+			//return !album.equals(a1); //lol you can't use equals because it sorts it first
+			return !album.tracks[0].equals(a1.tracks[0]);
+		});
+		if(!result) throw new Error("Shuffling an album fails to generate unique permutations!");
+	},
+	async function sort({Album,tracks}){
+		let a1 = new Album({tracks});
 		let f = function(album,key,reversed=false){
 			album.sort(key,reversed);
 			let arr = album.tracks;
@@ -107,61 +131,108 @@ export default [
 		}
 		f(a1,"title");
 		f(a1,"track_num");
-		f(a1,"duration");
+		f(a1,"src");
 		f(a1,"title",true);
 		f(a1,"track_num",true);
-		f(a1,"duration",true);
-		return Promise.resolve();
+		f(a1,"src",true);
 	},
-	function conversion(ALbum,obj){
-		let a1 = new Album(obj);
-		//a1.tracks[2] is type BC
+	async function clone({Album,tracks}){
+		let a1 = new Album({tracks});
+		a1.tracks.forEach(function(track){
+			if(track.filetype !== "BC") return;
+			track.src = "http://lol-no-one-cares";
+		});
 		//BC tracks overwrite their src when cloned
-		a1.tracks[2].src = "http://lol-no-one-cares";
-		let a2 = new Album(a1);
-		if(!a1.equals(a2)) throw new Error("Tracks are handled incorrectly");
-		return Promise.resolve();
+		let a2 = a1.clone();
+		for(let i = 0; i < tracks.length; i++){
+			if(tracks[i].src !== a2.tracks[i].src) throw new Error("Failed to clone track correctly!");
+		}
 	},
-	function addRemove(Album,obj){
-		let a1 = new Album(obj);
-		let a2 = new Album({title:obj.title});
-		a2.push(...obj.tracks);
-		//console.log(a1,a2);
-		if(!a1.equals(a2)) throw new Error("Push fails to add a track!");
-		a2.remove(...obj.tracks);
-		if(a2.length !== 0) throw new Error("Removing a track fails!");
-		console.log(a1,a2);
-		a2.insert(0,...obj.tracks);
-		if(!a1.equals(a2)) throw new Error("Insert fails to add a track");
-		return Promise.resolve();
+	async function filter({Album,tracks}){
+		let a1 = new Album({tracks});
+		let a2 = a1.clone();
+		let f = function(filetype){
+			return function(track){
+				return track.filetype === filetype;
+			}
+		}
+		let g = function(filetype){
+			let filtered = a1.filter(f(filetype));
+			if(!tracks.filter(f(filetype)).every(function(t,i,arr){
+				return filtered[i].equals(t);
+			})) throw new Error("Failed to filter selected tracks!");
+		}
+		g('HTML');
+		g('YT');
+		g('BC');
+		g('SC');
+		throwIfNotEqual("Failed to filter on a cloned album!",a1,a2);
 	},
-	function addRemoveAlbum(Album,obj){
-		let a1 = new Album(obj);
-		let a2 = new Album(obj);
-		a1.insert(0,a1);
-		a2.push(a2);
-		console.log(a1,a2);
-		if(!a1.equals(a2)) throw new Error("Adding an album fails!");
-		return Promise.resolve();
+	async function getInfo({Album,tracks}){
+		let a1 = new Album({tracks});
+		let f = function(key){
+			let arr = a1.getInfo(key);
+			if(!tracks.map(function(track){
+				return track[key];
+			}).every(function(t,i,arr){
+				return arr[i] === t;
+			})) throw new Error("Missing info!");
+		}
+		f("title");
+		f("src");
+		f("filetype");
+		let info = a1.getInfo("this_key_does_not_exist");
+		if(info.length !== 0) throw new Error("Failed to returned an empty array!");
 	},
-	function shuffle(Album,obj){
-		let a1 = new Album(obj);
-		let arr = Array.from({length: 64}, function(){
-			let tmp = a1.clone();
-			tmp.shuffle();
-			return tmp;
+	async function validTrack({Album,tracks}){
+		let tmp = {title:"title",src:"src",filetype:"THIS_FILETYPE_DOES_NOT_EXIST"};
+		if(Album.isValidTrack(tmp)) throw new Error("Failed to reject invalid track!");
+		Object.values(Album.players).forEach(function(player){
+			let track = new player.Track({title:"title",src:"src"});
+			if(!Album.isValidTrack(track)) throw new Error("Failed to accept valid track!");
 		});
-		let result = arr.some(function(album){
-			//return !album.equals(a1); //lol you can't use equals because it sorts it first
-			return !album.tracks[0].equals(a1.tracks[0]);
-		});
-		if(!result) throw new Error("Shuffling an album fails to generate unique permutations!");
-		return Promise.resolve();
 	},
-	function trackNum(Album,obj){
-		let a1 = new Album(obj);
-		let a2 = new Album({...obj,...{_unsorted:true}});
-
-		return Promise.resolve();
+	async function events({Album,tracks}){
+		let check = {
+			add:false, //push,insert
+			remove:false, //remove
+			clear:false, //clear
+			shuffle:false, //shuffle
+			sort:false, //sort
+		}
+		let f = function(evt){
+			console.debug(evt);
+			check[evt.type] = true;
+		}
+		let a1 = new Album();
+		a1.subscribe({type:'all',callback:f});
+		a1.push(...tracks);
+		a1.remove(tracks[0]);
+		a1.clear();
+		a1.insert(0,...tracks);
+		a1.shuffle();
+		a1.sort("title");
+		for(let evt in check){
+			if(!check[evt]) throw new Error("Failed to fire event: "+evt);
+		}
 	},
-]
+]);
+async function throwsError(f,...args){
+	try{
+		await f(...args);
+		return false;
+	}catch(e){
+		return true;
+	}
+}
+async function throwIfNoError(message,f,...args){
+	if(!await throwsError(f,...args)) throw new Error(message);
+}
+function throwIfNotEqual(message,...items){
+	for(let i = 1; i < items.length; i++){
+		if(items[i-1].equals && items[i-1].equals(items[i])) continue;
+		if(items[i].equals && items[i].equals(items[i-1])) continue;
+		if(items[i-1] === items[i]) continue;
+		throw new Error(message);
+	}
+}
--- tests/albums/js/main.js
+++ tests/albums/js/main.js
@@ -3,35 +3,37 @@ import YT from '../../../src/plugins/YT/youtube.js';
 import BC from '../../../src/plugins/BC/bandcamp.js';
 import SC from '../../../src/plugins/SC/soundcloud.js';
 import Album from '../../../src/album.js';
+//import Album from '../../../src/new-album.js';
 import Test from '../../shared/test.js';
 import Cases from './cases.js';
-//Put in global scope for easy debugging
-let imports = [HTML,YT,BC,SC,Album,Test,Cases];
-function map(arr,obj={},f=function(i){return i}){arr.forEach(function(i){if(i.name) this[i.name] = f(i)}.bind(obj));return obj;}
-map(imports,window);
-window.Cases = Cases;
-console.log("Loaded");
 
-let start_btn = document.getElementById("start_btn");
-let disable_console = document.getElementById("option-disable-console");
-start_btn.addEventListener("click",function(){ //need to wait for user interaction
-	test.clear();
-	for(let f of Cases){
-		if(test_cases[f.name]) test.add({f:f,args:args});
-	}
-	test.runAll(disable_console.checked);
-});
+//Put in global scope for easy debugging
+let imports = {HTML,YT,BC,SC,Album,Test,Cases};
+function map(src,dest={},key=function(k){return k},value=function(v){return v}){for(let k in src){dest[key(k)] = value(src[k]);};return dest;}
+map(imports,window);
+console.log("Imports Loaded");
 
 let t1 = new HTML.Track({src:"https://v.redd.it/6m47mro5xpv51/DASH_audio.mp4",title:"Scott's Factory"});
 let t2 = new YT.Track({src:"https://www.youtube.com/watch?v=zhG7aorm0RI",title:"Maynard & Waynard"});
 let t3 = new BC.Track({src:"https://austinwintory.bandcamp.com/track/then-were-created-the-gods-in-the-midst-of-heaven",title:"Then were created the gods in the midst of Heaven"});
 let t4 = new SC.Track({src:"https://soundcloud.com/i-winxd/kirby-speedrun",title:"Trance Music for Kirby Speedrunning Game"});
 
-Album.players = map([HTML,YT,BC,SC],{})
-let args = [Album,{title:"album",tracks:[t1,t2,t3,t4]}];
+Album.players = {HTML,YT,BC,SC};
+let args = {Album,tracks:[t1,t2,t3,t4]};
 let test = new Test();
 
-let test_cases = map(Cases,{},function(i){return true});
+let start_btn = document.getElementById("start_btn");
+start_btn.addEventListener("click",function(){ //need to wait for user interaction
+	test.clear();
+	for(let f of Cases){
+		test.add({f,args,skip:!test_cases[f.name]});
+	}
+	test.runAll();
+});
+
+
+
+let test_cases = map(Cases,{},function(k){return Cases[k].name},function(v){return true});
 
 //imported from shared/options.js folder
 createOptions(test_cases,"test_cases");
