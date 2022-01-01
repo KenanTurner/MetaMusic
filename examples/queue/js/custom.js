ee2e9a6558df47cbd0e41cc428654196ed21c1f1
KenanTurner <coolspykee@satx.rr.com>
2022-01-01 17:08:08 -0600

    Updated queue example

--- examples/queue/js/custom.js
+++ examples/queue/js/custom.js
@@ -1,40 +1,34 @@
 import _Track from '../../../src/track.js';
 export default class Track extends _Track{
-	static MetaMusic;
 	constructor(obj){
 		super(obj);
-		this.filetype = "CUSTOM";
 		this.elements = [];
 	}
 	clone(){
-		let tmp = this.constructor.fromJSON(JSON.stringify(this));
-		this.elements.forEach(function(el){
-			tmp.elements.push(el);
-		});
-		return tmp;
+		let track = super.clone();
+		track.elements = this.elements;
+		return track;
 	}
 	static fromJSON(json){ //deserialization
-		//TODO is this order correct?
 		let obj = {...super.fromJSON(json),...JSON.parse(json)}; //merge the two objects
 		return new Track(obj);
 	}
 	toHTML(){
 		let el = document.createElement('button');
 		el.innerText = this.title;
-		el.addEventListener('click',this.onclick.bind(this));
+		el.addEventListener('click',this.constructor.onclick.bind(this));
 		this.elements.push(el);
 		return el;
-	}
-	onclick(){
-		let mm = this.constructor.MetaMusic;
-		let paused = mm._status.paused;
-		mm.load(this).then(function(){
-			if(!paused) mm.play();
-		});
 	}
 	updateHTML(){
 		this.elements.forEach(function(el){
 			el.innerText = this.title;
 		}.bind(this));
 	}
+	css(f='toggle',css_class){
+		this.elements.forEach(function(e){
+			e.classList[f](css_class);
+		});
+	}
+	static async onclick(){} //To be overloader elsewhere
 }
--- examples/queue/js/main.js
+++ examples/queue/js/main.js
@@ -1,115 +1,81 @@
-ModuleManager.importModules({
-	"Custom":['./examples/queue/js/custom.js'],
-	"HTML":['./src/plugins/HTML/html.js'],
-	"YT":['./src/plugins/YT/youtube.js'],
-	"BC":['./src/plugins/BC/bandcamp.js'],
-	"SC":['./src/plugins/SC/soundcloud.js'],
-	"MM":['./src/meta-music.js'],
-	"Album":['./src/album.js'],
-}).then(function(obj){
-	let HTML = obj.HTML.default;
-	let Custom = obj.Custom.default;
-	Object.setPrototypeOf(HTML.Track,Custom); //This is poggers
-	Object.setPrototypeOf(HTML.Track.prototype,Custom.prototype); //Like super poggers
-	//Now it goes HTML.Track > Custom > Track
-	let YT = obj.YT.default;
-	let BC = obj.BC.default;
-	let SC = obj.SC.default;
-	let MetaMusic = obj.MM.default;
-	let Album = obj.Album.default;
-	console.log("Loaded");
-	
-	MetaMusic.players = {"HTML":HTML,"YT":YT,"SC":SC,"BC":BC};
-	window.mm = new MetaMusic({tracks:[
-		new HTML.Track({title:"Scott's Factory",src:"https://v.redd.it/6m47mro5xpv51/DASH_audio.mp4"}),
-		new YT.Track({title:"Waynard & Maynard",src:"https://www.youtube.com/watch?v=zhG7aorm0RI"}),
-		new BC.Track({title:"Want you Gone - Portal 2",src:"https://the8bitbigband.bandcamp.com/track/want-you-gone-feat-benny-benack-iii-from-portal-2"}),
-		new SC.Track({title:"K.K. Cruisin' (Interstate 5)",src:"https://soundcloud.com/aivisura/kk-cruisin-interstate-5-remix"}),
-	]});
-	mm.subscribe('error',function(err){
-		console.log(err);
-		alert("There was an error playing the requested file");
-	});
-	mm.subscribe('all',function(e){console.log(e)});
-	Custom.MetaMusic = mm;
-	
-	let queue = document.getElementById("queue");
-	mm.tracks.forEach(function(t){
-		queue.appendChild(t.toHTML());
-	});
-	
-	let previous_btn = document.getElementById("previous");
-	let backward_btn = document.getElementById("backward");
-	let play_btn = document.getElementById("play");
-	let pause_btn = document.getElementById("pause");
-	let stop_btn = document.getElementById("stop");
-	let forward_btn = document.getElementById("forward");
-	let next_btn = document.getElementById("next");
-	
-	let load_promise = mm.waitForEvent('ready');
-	
-	previous_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('next',-1));
-	});
-	backward_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('fastForward',-5));
-	});
-	play_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('play'));
-	});
-	pause_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('pause'));
-	});
-	stop_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('stop'));
-	});
-	forward_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('fastForward',5));
-	});
-	next_btn.addEventListener('click',function(){
-		load_promise.then(mm.chain('next',1));
-	});
-	mm.subscribe('loaded',function(e){
-		mm.tracks.forEach(function(t){
-			t.elements.forEach(function(e){
-				e.classList.remove('playing');
-			});
-		});
-		let track = e.target._track;
-		track.elements.forEach(function(e){
-			e.classList.add('playing');
-		});
-	});
-	
-})
-function insertAfter(newNode, existingNode) {
-    existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
+import Player from '../../../src/player.js';
+import HTML from '../../../src/plugins/HTML/html.js';
+import YT from '../../../src/plugins/YT/youtube.js';
+import BC from '../../../src/plugins/BC/bandcamp.js';
+import SC from '../../../src/plugins/SC/soundcloud.js';
+import MetaMusic from '../../../src/meta-music.js';
+import Custom from './custom.js';
+
+let imports = {Player,HTML,YT,BC,SC,MetaMusic,Custom};
+function map(src,dest={},key=function(k){return k},value=function(v){return v}){for(let k in src){dest[key(k)] = value(src[k]);};return dest;}
+map(imports,window);
+console.log("Imports Loaded");
+
+//Inject the Custom class into the prototype chain
+Object.setPrototypeOf(Player.Track,Custom); //This is poggers
+Object.setPrototypeOf(Player.Track.prototype,Custom.prototype); //Like super poggers
+
+MetaMusic.players = {HTML,YT,SC,BC};
+
+window.mm = new MetaMusic();
+mm.subscribe({type:'error',callback:function(err){
+	console.error(err);
+	alert("There was an error playing the requested file");
+}});
+mm.subscribe({type:'all',callback:function(e){console.debug(e)}});
+
+let tracks = [
+	new HTML.Track({title:"Scott's Factory",src:"https://v.redd.it/6m47mro5xpv51/DASH_audio.mp4"}),
+	new YT.Track({title:"Waynard & Maynard",src:"https://www.youtube.com/watch?v=zhG7aorm0RI"}),
+	new BC.Track({title:"Want you Gone - Portal 2",src:"https://the8bitbigband.bandcamp.com/track/want-you-gone-feat-benny-benack-iii-from-portal-2"}),
+	new SC.Track({title:"K.K. Cruisin' (Interstate 5)",src:"https://soundcloud.com/aivisura/kk-cruisin-interstate-5-remix"}),
+];
+mm.queue.push(...tracks);
+
+Custom.onclick = function(){
+	mm.load(this);
 }
-function swap(node1, node2) {
-    const afterNode2 = node2.nextElementSibling;
-    const parent = node2.parentNode;
-    node1.replaceWith(node2);
-    parent.insertBefore(node1, afterNode2);
+
+let previous_btn = document.getElementById("previous");
+let backward_btn = document.getElementById("backward");
+let play_btn = document.getElementById("play");
+let pause_btn = document.getElementById("pause");
+let stop_btn = document.getElementById("stop");
+let forward_btn = document.getElementById("forward");
+let next_btn = document.getElementById("next");
+
+previous_btn.addEventListener('click',mm.next.bind(mm,-1));
+backward_btn.addEventListener('click',mm.fastForward.bind(mm,-5));
+play_btn.addEventListener('click',mm.play.bind(mm));
+pause_btn.addEventListener('click',mm.pause.bind(mm));
+stop_btn.addEventListener('click',mm.stop.bind(mm));
+forward_btn.addEventListener('click',mm.fastForward.bind(mm,5));
+next_btn.addEventListener('click',mm.next.bind(mm,1));
+
+let is_paused = true;
+function isPaused(bool){
+	return function(e){
+		is_paused = bool;
+	}
 }
-let dragObj = {}
-function dragStart(e){
-	dragObj['target'] = e.target;
-	//e.target.parentNode.
-}
-function dragEnter(e){
-	e.preventDefault();
-}
-function dragOver(e){
-	e.preventDefault();
-	//console.log(e);
-	//if(el.parentNode.firstElementChild == el)
-}
-function dragLeave(e){
-	//console.log("LEAVE")
-}
-function dragDrop(e){
-	//console.log("DROP")
-	//dragObj['target'].style.opacity = 1;
-	insertAfter(dragObj['target'],e.target);
-	e.preventDefault();
-}
+
+play_btn.addEventListener('click',isPaused(false));
+pause_btn.addEventListener('click',isPaused(true));
+stop_btn.addEventListener('click',isPaused(true));
+mm.subscribe({type:'done',callback:isPaused(true)});
+
+mm.subscribe({type:'loaded',callback:function(e){
+	mm.queue.tracks.forEach(function(t){
+		t.css('remove','playing');
+	});
+	mm.current_track.css('add','playing');
+	if(!is_paused) mm.play();
+}});
+
+let queue = document.getElementById("queue");
+mm.queue.tracks.forEach(function(t){
+	queue.appendChild(t.toHTML());
+});
+
+await mm.waitForEvent('ready');
+mm.load(tracks[0]);
