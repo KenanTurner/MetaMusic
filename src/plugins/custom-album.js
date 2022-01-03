7509f96d35c6f1bbd943d3132638b8dd0ce08b8a
KenanTurner <coolspykee@satx.rr.com>
2022-01-02 19:41:43 -0600

    Updated custom Track/Album

--- src/plugins/custom-album.js
+++ src/plugins/custom-album.js
@@ -7,10 +7,9 @@ export default class Album extends A{
 		this.artwork_url = obj.artwork_url;
 		this.album_url = obj.album_url;
 		this.flags = obj.flags;
-		this._upload_date = obj._upload_date;
-		if(!obj._upload_date) this._upload_date = (new Date()).toJSON();
-		this._user_id = obj._user_id;
-		if(!obj._user_id) this._user_id = Album.getUserId();
+		this._upload_date = obj._upload_date? obj._upload_date: (new Date()).toJSON();
+		this._user_id = obj._user_id? obj._user_id: Album.getUserId(this);
+		this.elements = [];
 	}
 	toJSON(){ //serialization
 		let obj = super.toJSON();
@@ -23,11 +22,42 @@ export default class Album extends A{
 		obj._user_id = this._user_id;
 		return obj;
 	}
+	clone(){
+		let tmp = this.constructor.fromJSON(JSON.stringify(this));
+		tmp.elements = this.elements;
+		return tmp;
+	}
 	static fromJSON(json){ //deserialization
 		let obj = {...JSON.parse(json),...super.fromJSON(json)}; //merge the two objects
 		return new Album(obj);
 	}
-	static getUserId(){ //override later
-		return "TODO Override getUserId";
+	toHTML(){
+		let album_div = document.createElement('div');
+		album_div.classList.add('album');
+		album_div.title = this.title;
+			let album_img_div = document.createElement('div');
+			album_img_div.classList.add('album-img-container');
+				let album_img = document.createElement('img');
+				album_img.classList.add('album-img');
+				album_img.src = this.artwork_url || "./images/default-white.png";
+				album_img_div.appendChild(album_img);
+			album_div.appendChild(album_img_div);
+			let album_text_div = document.createElement('div');
+			album_text_div.classList.add('album-text-container');
+				let album_title = document.createElement('div');
+				album_title.classList.add('album-title');
+				album_title.innerText = this.title;
+				album_text_div.appendChild(album_title);
+			album_div.appendChild(album_text_div);
+		album_div.addEventListener('click',this.constructor.onClick.bind(null,this));
+		return album_div;
 	}
+	css(f='toggle',css_class){
+		this.elements.forEach(function(e){
+			e.classList[f](css_class);
+		});
+	}
+	//to be overloaded later
+	static onClick(album){}
+	static getUserId(album){}
 }
--- src/plugins/custom.js
+++ src/plugins/custom.js
@@ -1,18 +1,15 @@
-import _Track from '../track.js';
-export default class Track extends _Track{
+import T from '../track.js';
+export default class Track extends T{
 	constructor(obj){
 		super(obj);
-		this.filetype = "CUSTOM";
 		this.duration = obj.duration;
 		this.artist = obj.artist;
 		this.artwork_url = obj.artwork_url;
 		this.flags = obj.flags;
 		this._total_views = obj._total_views;
 		this._total_likes = obj._total_likes;
-		this._upload_date = obj._upload_date;
-		if(!obj._upload_date) this._upload_date = (new Date()).toJSON();
-		this._user_id = obj._user_id;
-		if(!obj._user_id) this._user_id = Track.getUserId();
+		this._upload_date = obj._upload_date? obj._upload_date: (new Date()).toJSON();
+		this._user_id = obj._user_id? obj._user_id: Track.getUserId(this);
 		this.elements = [];
 	}
 	toJSON(){ //serialization
@@ -29,18 +26,12 @@ export default class Track extends _Track{
 	}
 	clone(){
 		let tmp = this.constructor.fromJSON(JSON.stringify(this));
-		this.elements.forEach(function(el){
-			tmp.elements.push(el);
-		});
+		tmp.elements = this.elements;
 		return tmp;
 	}
 	static fromJSON(json){ //deserialization
-		//TODO is this order correct?
 		let obj = {...super.fromJSON(json),...JSON.parse(json)}; //merge the two objects
 		return new Track(obj);
-	}
-	static getUserId(){ //override later
-		return "TODO Override getUserId";
 	}
 	toHTML(){
 		let el = document.createElement('button');
@@ -49,13 +40,12 @@ export default class Track extends _Track{
 		this.elements.push(el);
 		return el;
 	}
-	updateHTML(){
-		this.elements.forEach(function(el){
-			el.innerText = this.title;
-		}.bind(this));
+	css(f='toggle',css_class){
+		this.elements.forEach(function(e){
+			e.classList[f](css_class);
+		});
 	}
 	//to be overloaded later
-	static onClick(){}
-	static onLoad(){}
-	static onUnload(){}
+	static onClick(track){}
+	static getUserId(track){}
 }
