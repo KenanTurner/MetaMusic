5caf25de54caba44ff000e842602e5ac0455b668
KenanTurner <coolspykee@satx.rr.com>
2022-04-14 10:12:37 -0500

    Overhauled Test class

--- tests/shared/test.js
+++ tests/shared/test.js
@@ -1,74 +1,55 @@
-export default class Test{
+import AsyncQueue from '../../src/async-queue.js';
+export default class Test extends AsyncQueue{
 	constructor(){
+		super(0);
 		this.results = {pass:0,fail:0,skip:0};
-		this.test_cases = [];
-	}
-	clear(){
-		this.test_cases.length = 0;
-		this.reset();
-	}
-	add(obj){
-		if(Array.prototype.isPrototypeOf(obj)) return obj.forEach(function(o){
-			this.add(o);
-		}.bind(this));
-		if(typeof(obj.f) !== 'function') throw new Error("Not a function!");
-		if(!obj.args) obj.args = {};
-		this.test_cases.push(obj);
-	}
-	remove(obj){
-		if(Array.prototype.isPrototypeOf(obj)) return obj.forEach(function(o){
-			this.remove(o);
-		}.bind(this));
-		if(typeof(obj.f) !== 'function') throw new Error("Not a function!");
-		if(!obj.args) obj.args = {};
-		this.test_cases = this.test_cases.filter(function(_obj){
-			if(JSON.stringify(obj) === JSON.stringify(_obj) && obj.f.toString() === _obj.f.toString()) return false;
-			return true;
-		});
 	}
 	reset(){
 		this.results.pass = 0;
 		this.results.fail = 0;
 		this.results.skip = 0;
 	}
-	async runAll(){
-		this.reset();
-		console.log("Running Tests...");
-		await this._run(this.test_cases);
+	async enqueue({f,args,skip,message,timeout,name}){
+		let test_name = name || f.name || this._async_queue.length;
+		let g = async function(){
+			return new Promise(async function(res,rej){
+				if(skip){
+					this.results.skip++;
+					console.log(`  ${message||""} ${test_name}: %cSkipped`,"color: #3272fc;");
+					return res(this.results);
+				}
+				try{
+					let p = f(args || {});
+					if(!Promise.prototype.isPrototypeOf(p)) throw Error("Failed to return a promise!");
+					let e = await Promise.race([p,new Promise(function(_res, _rej){
+						setTimeout(_rej.bind(null,"Timed Out!"), timeout || 10000);
+					})]);
+					this.results.pass++;
+					console.log(`  ${message||""} ${test_name}: %cPassed`,"color: #00FF2D;");
+				}catch(e){
+					this.results.fail++;
+					console.log(`  ${message||""} ${test_name}: %cFailed: `,"color: #FF0000;",e);
+				}finally{
+					res(this.results);
+				}
+			}.bind(this));
+		}.bind(this);
+		return super.enqueue(g);
+	}
+	async run(max_concurrent_tests = 1){
+		console.log(`Running ${this._async_queue.length} Tests...`);
+		let p = Promise.allSettled(this._pending_promises);
+		this._max_concurrent_promises = max_concurrent_tests;
+		for(let i = 0; i < max_concurrent_tests; i++) this.dequeue();
+		await p;
+		this._max_concurrent_promises = 0;
 		console.log("%cPassed Cases: ","color: #00FF2D;",this.results.pass);
 		console.log("%cFailed Cases: ","color: #FF0000;",this.results.fail);
 		console.log("%cSkipped Cases: ","color: #3272fc;",this.results.skip);
 		return this.results;
 	}
-	run(obj){
-		try{
-			let p = obj.f(obj.args);
-			if(!Promise.prototype.isPrototypeOf(p)) return Promise.reject("Test Case "+obj.f.name+" must return a promise!");
-			let t = new Promise(function(resolve, reject) {
-				setTimeout(function(){reject("Timed out!")}, obj.timeout || 10000);
-			});
-			return Promise.race([p, t]);
-		}catch(err){
-			return Promise.reject(err);
-		}
-	}
-	_run(cases,index=0){ //recursively test each case
-		if(cases.length == index) return Promise.resolve("No more cases!");
-		let obj = cases[index];
-		console.log("Testing "+obj.f.name+": "+(obj.message||""));
-		if(obj.skip){
-			console.log("%cSkipped","color: #3272fc;");
-			this.results.skip++;
-			return this._run(cases,index+1);
-		}
-		return this.run(obj).then(function(){
-			console.log("%cPassed","color: #00FF2D;");
-			this.results.pass++;
-		}.bind(this),function(result){
-			console.log("%cFailed: ","color: #FF0000;",result);
-			this.results.fail++;
-		}.bind(this)).finally(function(){
-			return this._run(cases,index+1);
-		}.bind(this));
+	async clear(){
+		super.clear(true);
+		this.reset();
 	}
 }
