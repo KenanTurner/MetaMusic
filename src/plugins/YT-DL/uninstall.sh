8a00f55155d05c242dd192cab135811d902c6393
KenanTurner <coolspykee@satx.rr.com>
2022-05-03 13:00:27 -0500

    Updated uninstall script

--- src/plugins/YT-DL/uninstall.sh
+++ src/plugins/YT-DL/uninstall.sh
@@ -1,5 +1,7 @@
+echo "Uninstalling youtube-dl.servce..."
 DirName=$(pwd)
 systemctl --user stop youtube-dl
 systemctl --user disable youtube-dl
 systemctl --user daemon-reload
 rm ~/.config/systemd/user/youtube-dl.service
+echo "Done!"
