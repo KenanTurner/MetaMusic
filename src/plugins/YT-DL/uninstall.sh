4e612ff0f1a8d8c5cb42f566946d66b7fa1fefec
KenanTurner <coolspykee@satx.rr.com>
2022-05-04 12:49:01 -0500

    No more background processes :(

--- src/plugins/YT-DL/uninstall.sh
+++
@@ -1,7 +0,0 @@
-echo "Uninstalling youtube-dl.servce..."
-DirName=$(pwd)
-systemctl --user stop youtube-dl
-systemctl --user disable youtube-dl
-systemctl --user daemon-reload
-rm ~/.config/systemd/user/youtube-dl.service
-echo "Done!"
--- src/plugins/YT-DL/youtube-dl.php
+++ src/plugins/YT-DL/youtube-dl.php
@@ -8,19 +8,6 @@
 	$url = $track["src"];
 	if(empty($url) or filter_var($url, FILTER_VALIDATE_URL) === FALSE) error("invalid URL!");
 	
-	$result = '';
-	$address = 'localhost';
-	$port = 4200;
-	$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-	if(!$socket) error('Unable to create socket!');
-	if(!socket_connect($socket, $address, $port)) error('Unable to connect to socket!');
-	if(!socket_write($socket, json_encode($track), 8192)) error('Failed to write to socket!');
-	while($chunk = socket_read($socket, 8192)){
-		if(!$chunk) error('Failed to read from socket!');
-		$result .= $chunk;
-	}
-	socket_close($socket);
-	if(empty($result)) error('Unable to resolve URL!');
-	
-	echo $result;
+	$args = ["python3 -m yt-dlp","'".json_encode($track)."'"];
+	passthru(join(" ",$args));
 ?>
