eac257c77e34534c148720a15b1aad86ec933457
KenanTurner <coolspykee@satx.rr.com>
2022-05-31 23:23:24 -0500

    Updated console

--- tests/shared/console.js
+++ tests/shared/console.js
@@ -1,149 +1,102 @@
-export default class Console{
+import EventTarget from '/src/event-target.js';
+import Component from '/tests/shared/component.js';
+export default class ConsoleComponent extends Component{
+	static template = document.createElement('template');
 	constructor(){
-		if(window.top.console._console) return window.top.console;
-		let options = {debug:false,log:true,warn:true,error:true};
-		this._options = new Proxy(options,{
-			set(target,prop,value){
-				if(value === true) window.top.console.show(prop);
-				if(value === false) window.top.console.hide(prop);
-				return Reflect.set(...arguments);
-			}
-		});
-		this._console = {};
-		for(let f in window.top.console){
-			this._console[f] = window.top.console[f];
-			Reflect.set(this,f,window.top.console[f]);
-		}
-		this._div = document.createElement("div");
-		this._div.classList.add("console");
-		this._div.id = "console";
-		this._output = this.output();
-		this._div.appendChild(this._output);
-		this._controls = this.controls();
-		this._div.appendChild(this._controls);
-		return new Proxy(this,this);
-	}
-	get(target,prop,receiver){
-		if(prop === 'clear'){
-			return function(...data){
-				while(this._output.firstChild){
-					this._output.removeChild(this._output.lastChild);
-				}
-				this._console[prop](...data);
-			}
-		}
-		if(this._console[prop]){
-			return function(...data){
-				let div = this.toHTML(...data);
-				div.classList.add(prop);
-				if(this._options[prop] === false) div.classList.add('hidden');
-				this._output.appendChild(div);
-				this._console[prop](...data);
-				this._controls.scrollIntoView();
-			}.bind(this);
-		}
-		return Reflect.get(...arguments);
-	}
-	toHTML(...data){
-		let divs = data.map(function(item){
-			let pre = document.createElement('pre');
-			pre.innerText = this.toText(item);
-			return pre;
+		super();
+		this.output = this.shadowRoot.querySelector("#output");
+		this.input = this.shadowRoot.querySelector("#input");
+		this.eval = this.shadowRoot.querySelector("#eval");
+		this.command = this.shadowRoot.querySelector("#command");
+		this.eval.addEventListener("click",function(){
+			if(this.command.value === "") return;
+			window.top.console.log(this.command.value);
+			this.exec(this.command.value);
+			this.command.value = "";
 		}.bind(this));
-		if(data.length > 0 && typeof data[0] === 'string' && /%c/.test(data[0])){
-			let arr = data[0].split(/%c/);
-			divs[0].innerText = arr[0];
-			for(let i = 1; i < arr.length; i++){
-				if(!data[i]) continue;
-				divs[i].style = data[i];
-				divs[i].innerText = arr[i];
-			}
-		}
-		let div = document.createElement('div');
-		divs.forEach(function(pre){
-			div.appendChild(pre);
-		});
-		return div;
-	}
-	toText(item){
-		switch(typeof item){
-			case "string":
-				if(item === '') return '\n';
-				return item;
-			case "function":
-				return item.toString();
-			case "object":
-				if(Error.prototype.isPrototypeOf(item)){
-					return item.stack;
-				}else if(item){
-					return item.constructor.name + " " + JSON.stringify(item,null,'\t');
-				}
-			default:
-				return JSON.stringify(item,null,'\t');
-		}
-		return "ERROR CONVERTING ITEM";
-	}
-	controls(){
-		let controls = document.createElement("div");
-		controls.classList.add("controls");
-		controls.id = "console-controls";
-		let btn = document.createElement("button");
-		btn.innerText = ">";
-		btn.classList.add("eval");
-		btn.addEventListener("click",function(){
-			if(input.value === "") return;
-			window.top.console.log(input.value);
-			this.eval(input.value);
-			input.style.height = "15px";
-			input.value = "";
-		}.bind(this));
-		let input = document.createElement("textarea");
-		input.style.height = "15px";
-		input.rows = "1";
-		input.autocomplete = "off";
-		input.autocorrect = "off";
-		input.autocapitalize = "off";
-		input.spellcheck = false;
-		input.wrap = "soft";
-		input.classList.add("input");
-		input.addEventListener("keydown",function(e){
+		this.command.addEventListener("keydown",function(e){
 			if(e.key === 'Enter' || e.keyCode === 13){
-				if(e.shiftKey){
-					input.style.height = Number(input.style.height.slice(0,-2))+15+"px";
-				}else{
-					event.preventDefault();
-					btn.click();
-				}
+				this.eval.click();
 			}
 		}.bind(this));
-		controls.appendChild(btn);
-		controls.appendChild(input);
-		return controls;
 	}
-	output(){
-		let output = document.createElement("div");
-		output.classList.add("output");
-		output.id = "console-output";
-		return output;
-	}
-	eval(str){
+	exec(str){
 		try{
-			let t = window.top.eval(str); //super terrible but I don't care
+			let t = window.top.eval(str); //may God have mercy on my soul
 			window.top.console.log(t);
 		}catch(e){
 			window.top.console.error(e);
 		}
 	}
-	show(level){
-		for(let div of Array.from(this._output.children)){
-			if(!div.classList.contains(level)) continue;
-			div.classList.remove('hidden');
-		}
-	}
-	hide(level){
-		for(let div of Array.from(this._output.children)){
-			if(!div.classList.contains(level)) continue;
-			div.classList.add('hidden');
-		}
-	}
 }
+ConsoleComponent.template.innerHTML = await(await fetch('/tests/shared/console.html')).text();
+customElements.define('console-component',ConsoleComponent);
+
+const console = EventTarget.observe(window.top.console);
+const container = new ConsoleComponent();
+
+const params = new URLSearchParams(window.location.search);
+const DISPLAY_LOG = Boolean(params.get('log')) || true;
+const DISPLAY_ERROR = Boolean(params.get('error')) || true;
+const DISPLAY_WARN = Boolean(params.get('warn')) || true;
+const DISPLAY_DEBUG = Boolean(params.get('debug')) || false;
+const DISPLAY_CONSOLE = Boolean(params.get('console')) || window.location.pathname.includes("tests");
+console.options = EventTarget.observe({log:DISPLAY_LOG,error:DISPLAY_ERROR,warn:DISPLAY_WARN,debug:DISPLAY_DEBUG});
+console.options.observer.subscribe("all",{callback:function({type,state}){
+	container.shadowRoot.querySelectorAll("."+type).forEach(function(node){
+		state[type]? node.classList.remove("hidden"): node.classList.add("hidden");
+	});
+}});
+
+function toText(item){
+	switch(typeof item){
+		case "string":
+			return item;
+		case "function":
+			return item.toString();
+		case "object":
+			if(Error.prototype.isPrototypeOf(item)){
+				return item.stack;
+			}else if(item){
+				return item.constructor.name + " " + JSON.stringify(item,null,'\t');
+			}
+		default:
+			return JSON.stringify(item,null,'\t');
+	}
+	return `ERROR: unable to convert ${item} to text!`;
+}
+function toHTML(...args){
+	return args.reduce(function(arr,item,index){
+		if(index === 0 && typeof item === 'string' && /%c/.test(item)){
+			return item.split(/%c/).map(function(text,i){
+				let pre = document.createElement('pre');
+				pre.innerText = toText(text);
+				pre.style = args[i];
+				if(i > 0) args.splice(1,1); //remove style tag from args
+				return pre;
+			});
+		}
+		let pre = document.createElement('pre');
+		pre.innerText = toText(item);
+		arr.push(pre);
+		return arr;
+	},[]).reduce(function(div,el){
+		div.appendChild(el);
+		return div;
+	},document.createElement("div"));
+}
+
+let listener = {callback:function({type,state,args}){
+	let node = toHTML(...args);
+	node.classList.add(type);
+	if(!state.options[type]) node.classList.add("hidden");
+	container.output.appendChild(node);
+}};
+console.observer.subscribe("log",listener);
+console.observer.subscribe("warn",listener);
+console.observer.subscribe("error",listener);
+console.observer.subscribe("debug",listener);
+
+window.top.console = console;
+if(DISPLAY_CONSOLE) document.body.appendChild(container);
+console.log("Console Loaded");
